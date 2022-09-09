2c86a933e218c5e295a6cc1bd66749f1059e15d5
Talitha Brown <talithabrown128@gmail.com>
2022-09-09 11:01:43 -0700

    added localhost 8000 to cors allowed orgins

--- hungry_backend_api/settings/common.py
+++ hungry_backend_api/settings/common.py
@@ -66,6 +66,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8001',
     'http://127.0.0.1:8001',
     'http://localhost:3000',
+    'http://localhost:8000',
 ]
 
 ROOT_URLCONF = 'hungry_backend_api.urls'
